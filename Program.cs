0af5fefaf9a517593e8c291648e3d0f8a8807d1c
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-08-05 14:19:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SumDifMultDiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите 1-е число: ");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("Введите 2-е число: ");
+                int b = int.Parse(Console.ReadLine());
+                Console.WriteLine($"\nСумма: {a + b}\nРазница: {a - b}\nПроизведение: {a * b}\nЧастное: {a / b}\nОстаток от деления: {a % b}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("На ноль делить нельзя!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
